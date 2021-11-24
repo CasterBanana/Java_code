eea44ef5229bbe500d4cc388fb8f0de5d1560914
CasterBanana <94629775+CasterBanana@users.noreply.github.com>
2021-11-24 21:44:38 +0400

    Тесты
    Написал два теста

---
+++ sandbox/src/test/java/ru/kruto/sandbox/SquareTests.java
@@ -0,0 +1,15 @@
+package ru.kruto.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+    @Test // аннотация её пишем либо @test, либо org.testng.annotations.Test, но в таком случае импорт не делается
+    public void testArea() { // у этого тестового метода нет никакого возвращаемого результата
+        // и параметров, а результат сообщает тестовому фреймворку
+        Square s = new Square(5);
+        assert s.area() == 24;
+        //Assert.assertEquals(s.area(),25);
+
+    }
+}
