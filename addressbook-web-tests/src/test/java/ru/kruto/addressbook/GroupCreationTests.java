f9b46dae55d3888fa65b93530ace6361c2a6d0ae
CasterBanana <94629775+CasterBanana@users.noreply.github.com>
2021-11-28 22:21:45 +0400

    Правки в тесте и фикстуре
    Перенёс авторизацию в BeforeMethod

--- addressbook-web-tests/src/test/java/ru/kruto/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/kruto/addressbook/GroupCreationTests.java
@@ -13,15 +13,11 @@ import org.apache.commons.io.FileUtils;
 public class GroupCreationTests {
   private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true) //фикстура до метода
   public void setUp() throws Exception {
     //System.setProperty("webdriver.chrome.driver", "");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -31,6 +27,11 @@ public class GroupCreationTests {
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+
+  @Test
+  public void testGroupCreation() throws Exception { // не стоит называть метод, как класс
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
@@ -45,7 +46,7 @@ public class GroupCreationTests {
     wd.findElement(By.linkText("Logout")).click();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterMethod(alwaysRun = true) //фикстура после метода
   public void tearDown() throws Exception {
     wd.quit();
   }
