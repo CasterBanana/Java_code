50ab54432c3c1361b649c2399fdb3d89f2281062
CasterBanana <94629775+CasterBanana@users.noreply.github.com>
2021-12-16 19:12:39 +0400

    4.7

--- addressbook-web-tests/src/test/java/ru/kruto/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/kruto/addressbook/tests/GroupCreationTests.java
@@ -13,16 +13,8 @@ public class GroupCreationTests extends TestBase { // Теперь это нас
   public void testGroupCreation()  { // не стоит называть метод, как класс
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    //int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("2", "logo", "test"));//это из GroupHelper теперь
-    /*app.getGroupHelper().initGroupCreation(); // на всякий случай оставлю старый вариант, когда в тесте описывались шаги
-    //вызываем метод fillGroupForm и заполняем значения
-    app.getGroupHelper().fillGroupForm(new GroupData("Firefox", "logo", "test"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
-    //app.stop();*/
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    //int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(),before.size() + 1);
   }
 
