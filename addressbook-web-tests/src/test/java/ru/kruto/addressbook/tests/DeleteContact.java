2f31f38aff29a94d39f927228dede0eb651c148c
CasterBanana <94629775+CasterBanana@users.noreply.github.com>
2021-12-19 15:24:06 +0400

    Ещё правки

--- addressbook-web-tests/src/test/java/ru/kruto/addressbook/tests/DeleteContact.java
+++ addressbook-web-tests/src/test/java/ru/kruto/addressbook/tests/DeleteContact.java
@@ -9,13 +9,14 @@ public class DeleteContact extends TestBase {
 
     @Test
     public void testDeleteContact() throws Exception {
-        int before = app.getContactHelper().getContactCount();
+        //int before = app.getContactHelper().getContactCount();
         if (! app.getContactHelper().isThereAContact()){
             app.getContactHelper().initializationNewContact();
             app.getContactHelper().fillInfoNewContact(new ContactData("PepВЫВ1ega", "Kek", "88005553535", "omega_pepega@mail.com"));
             app.getContactHelper().confirmNewContact();
             app.returnToHomePage();
         }
+        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteContact();
         app.getContactHelper().getAcceptContact(); // подтверждение для всплыващющего окна
--- addressbook-web-tests/src/test/java/ru/kruto/addressbook/tests/EditingContact.java
+++ addressbook-web-tests/src/test/java/ru/kruto/addressbook/tests/EditingContact.java
@@ -9,13 +9,13 @@ public class EditingContact extends TestBase {
 
     @Test
     public void testEditContact() throws Exception { // что-то неправильно отрабатывает предусловие
-        int before = app.getContactHelper().getContactCount();
         if ( app.getContactHelper().isThereAContact()){
             app.getContactHelper().initializationNewContact();
             app.getContactHelper().fillInfoNewContact(new ContactData("PepВЫВ1ega", "Kek", "88005553535", "omega_pepega@mail.com"));
             app.getContactHelper().confirmNewContact();
             app.returnToHomePage();
         }
+        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().initializationEditContact();
         app.getContactHelper().editContact("Oleg12");
         app.getContactHelper().safeUpdateContact();
