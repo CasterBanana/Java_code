55237b5512e8f439b8e371f152f66b7628945b7c
CasterBanana <94629775+CasterBanana@users.noreply.github.com>
2022-01-16 23:43:42 +0400

    Первый тест работает

--- mantis-tests/src/test/java/ru.stqa.pft.mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru.stqa.pft.mantis/appmanager/HttpSession.java
@@ -37,7 +37,8 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params));
         CloseableHttpResponse response = httpClient.execute(post);
         String body = geTextForm(response);
-        return body.contains(String.format("<span id=\"logged-in-user\">%s</span>", username));
+        //return body.contains(String.format("<span id=\"logged-in-user\">%s</span>", username));
+        return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
     }
 
     private String geTextForm(CloseableHttpResponse response) throws IOException {
@@ -52,7 +53,8 @@ public class HttpSession {
         HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
         CloseableHttpResponse response = httpClient.execute(get);
         String body = geTextForm(response);
-        return body.contains(String.format("<span id=\"logged-in-user\">%s</span>", username));
+        //return body.contains(String.format("<span id=\"logged-in-user\">%s</span>", username));
+        return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
     }
 }
 
