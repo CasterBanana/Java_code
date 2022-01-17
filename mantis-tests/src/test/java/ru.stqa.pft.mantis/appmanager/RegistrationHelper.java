b5f7c1b4420416d14a8ca66691b533ef28971195
CasterBanana <94629775+CasterBanana@users.noreply.github.com>
2022-01-17 20:22:24 +0400

    Падает

--- mantis-tests/src/test/java/ru.stqa.pft.mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru.stqa.pft.mantis/appmanager/RegistrationHelper.java
@@ -14,7 +14,9 @@ public class RegistrationHelper extends HelperBase {
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
         type(By.name("username"),username);
         type(By.name("email"), email);
-        click(By.cssSelector("input[value='Signup']"));
+        click(By.cssSelector("input[value='Зарегистрироваться']"));
+        //click(By.cssSelector("input[type='submit']"));// указание кнопки регистрации другое
+        //click(By.cssSelector("input[value='Signup']"));// старый вариант
 
     }
 
@@ -22,6 +24,7 @@ public class RegistrationHelper extends HelperBase {
         wd.get(confirmationLink);
         type(By.name("password"), password);
         type(By.name("password_confirm"), password);
+        //click(By.cssSelector("input[value='Изменить учетную запись']"));
         click(By.cssSelector("input[value='Update User']"));
 
     }
